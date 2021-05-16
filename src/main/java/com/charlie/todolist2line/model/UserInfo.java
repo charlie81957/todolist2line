37bd50aef0dd2b3a8e29bcd6cb031825efd151a8
mawasigeri <yamayui1107@i.softbank.jp>
2021-05-16 14:28:01 +0900

    [fix]コンストラクターを修正
    コンストラクターの宣言を追加

--- src/main/java/com/charlie/todolist2line/model/UserInfo.java
+++ src/main/java/com/charlie/todolist2line/model/UserInfo.java
@@ -1,9 +1,13 @@
 package com.charlie.todolist2line.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo{
     private String userId;
     private String encryptionPassword;
