75d0739547c8de58f929dc19af7baf326430fdb4
lake429c <have2nthusiasm@gmail.com>
2021-05-19 06:55:52 +0900

    [update] Todoのmodelをjpa用に変更

--- src/main/java/com/charlie/todolist2line/model/Todo.java
+++ src/main/java/com/charlie/todolist2line/model/Todo.java
@@ -1,16 +1,41 @@
 package com.charlie.todolist2line.model;
 
-
 import java.security.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class Todo{
-    private String usreId;
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "todo_info")
+public class Todo {
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Id
+    @Column(name = "todo_id")
     private String todoId;
+
+    @Column(name = "todo_title")
     private String todoTitle;
+
+    @Column(name = "todo_content")
     private String todoContent;
-    private boolean isDone;
+
+    @Column(name = "limit_datetime")
     Timestamp limitDateTime;
+
+    @Column(name = "is_done")
+    private boolean isDone;
+
 }
