3ea1bcd6d65c476efe0b9395f4ff8cc6ffaeb048
imu---ra <atusibba1014@yahoo.co.jp>
2021-05-29 15:13:00 +0900

    React導入

--- src/main/java/com/charlie/todolist2line/controller/UserController.java
+++ src/main/java/com/charlie/todolist2line/controller/UserController.java
@@ -5,13 +5,17 @@ import com.charlie.todolist2line.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+// @CrossOrigin // Edited by imura 2021-0523--https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
 @Controller
+// @RestController
 public class UserController {
     
     @Autowired
@@ -39,12 +43,23 @@ public class UserController {
      */
     @ResponseBody           
     @RequestMapping(value="/signIn", method=RequestMethod.POST)
-    public boolean SignIn(@RequestParam UserInfoDto userInfoDto) {
-        return userService.isAbelToLogin(userInfoDto);
+    @CrossOrigin
+    public boolean SignIn(@RequestBody UserInfoDto userInfoDto) {
+        // return userService.isAbelToLogin(userInfoDto);
+        return true; // EDIT!!!
     }
     
     @RequestMapping(value = "/loguot")
     public String logout(){
         return this.loginInit();
+    }
+
+    // 井村が作ったやつです。UserIdの存在確認用
+    @ResponseBody           
+    @RequestMapping(value="/isExist", method=RequestMethod.POST)
+    @CrossOrigin
+    public boolean isExistUserId(@RequestBody String uid) {
+        // return userService.isAbelToLogin(userInfoDto);
+        return true; // EDIT!!!
     }
 }
